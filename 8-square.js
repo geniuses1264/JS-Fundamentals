9d6084edc8896d364ba722dafe8f847a36b67cd8
Ebenezer Attipoe <kwameebenezer114@gmail.com>
2025-07-12 01:43:40 +0000

    feat: print square using loop in 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,11 +1,13 @@
-const x = process.argv.slice(2);
-const size = parseInt(x[0]);
+// 1. Get the input
+const args = process.argv.slice(2);
+const size = parseInt(args[0]);
 
-    if(isNaN(size)){
-        console.log("Missing number of occurrences");
-    }
-      else{
-        for(let j = 0; j < size; j++){
-            console.log("X".repeat(size));
-        }
-    }
+// 2. Validate it
+if (isNaN(size)) {
+  console.log("Missing size");
+} else {
+  // 3. Now safely run the loop
+  for (let i = 0; i < size; i++) {
+    console.log("X".repeat(size));
+  }
+}
